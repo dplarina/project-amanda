30f6ad5e399b78aeb75fc7cda99dcb832dacc50b
Daniel Plarina <dplarina@inspirehub.com>
2022-01-23 23:03:36 -0500

    Added application insights

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>();
 
