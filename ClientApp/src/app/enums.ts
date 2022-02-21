153a09e0eca9d0b7a519328f406b3c9f1b087714
Daniel Plarina <dplarina@inspirehub.com>
2022-02-21 13:15:52 -0500

    Added categories

--- ClientApp/src/app/enums.ts
+++ ClientApp/src/app/enums.ts
@@ -1,4 +1,13 @@
 export const ItemCategory = {
-  Vegetables: 1,
-  Fruits: 2
+  Bread: 1,
+  Refrigerated: 2,
+  Kids: 3,
+  Frozen: 4,
+  'Beverages/Drinks': 5,
+  Household: 6,
+  'Beauty products': 7,
+  'Beans/Grains/Nuts': 8,
+  Pantry: 9,
+  Pasta: 10,
+  Snacks: 11
 };
