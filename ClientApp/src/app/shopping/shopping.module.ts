c8a6ae919e36adf16ed2bc4124ba8c76a6c512f3
Daniel Plarina <dplarina@inspirehub.com>
2022-02-21 12:34:35 -0500

    Added flex-layout

--- ClientApp/src/app/shopping/shopping.module.ts
+++ ClientApp/src/app/shopping/shopping.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -32,7 +33,9 @@ import { StoreListComponent } from './store-list/store-list.component';
     MatFormFieldModule,
     MatProgressBarModule,
     MatToolbarModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+
+    FlexLayoutModule
   ]
 })
 export class ShoppingModule {}
